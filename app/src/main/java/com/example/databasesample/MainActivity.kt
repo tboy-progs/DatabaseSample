75a71576bb8e764a13a897aec7938a147400b342
taka <gluestick829@gmail.com>
2024-08-13 11:19:34 +0900

    Add:保存ボタン、リストビュー選択処理を追加

--- app/src/main/java/com/example/databasesample/MainActivity.kt
+++ app/src/main/java/com/example/databasesample/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.databasesample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var _cocktailId = -1
+    private var _cocktailName = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val lvCocktail = findViewById<ListView>(R.id.lvCocktail)
+        lvCocktail.onItemClickListener = ListItemClickListener()
+    }
+
+    fun onSaveButtonClick(view: View) {
+        val etNote = findViewById<EditText>(R.id.etNote)
+        etNote.setText("")
+
+        val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
+        tvCocktailName.text = getString(R.string.tv_name)
+
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        btnSave.isEnabled = false
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            _cocktailId = position
+            _cocktailName = parent.getItemAtPosition(position) as String
+
+            val tvCocktailName = findViewById<TextView>(R.id.tvLbName)
+            tvCocktailName.text = _cocktailName
+
+            val btnSave = findViewById<Button>(R.id.btnSave)
+            btnSave.isEnabled = true
         }
     }
 }
