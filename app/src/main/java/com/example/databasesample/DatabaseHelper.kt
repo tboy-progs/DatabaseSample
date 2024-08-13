aaa788c661ffd501d011e0f50b78b446d5b7b1f4
taka <gluestick829@gmail.com>
2024-08-13 12:41:49 +0900

    Add:データベースヘルパークラスにsqlの実行処理を実装

--- app/src/main/java/com/example/databasesample/DatabaseHelper.kt
+++ app/src/main/java/com/example/databasesample/DatabaseHelper.kt
@@ -1,4 +1,30 @@
 package com.example.databasesample
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "cocktailmemo.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val sb = StringBuilder()
+        sb.append("CREATE TABLE cocktailmemos(")
+        sb.append("_id INTEGER PRIMARY KEY,")
+        sb.append("name TEXT,")
+        sb.append("note TEXT")
+        sb.append(");")
+
+        val sql = sb.toString()
+
+        db.execSQL(sql)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
 }
